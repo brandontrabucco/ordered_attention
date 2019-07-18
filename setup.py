038451edf4df1d2dfb355ad22854b1e7eae19470
Brandon <brandon@btrabucco.com>
2019-07-18 00:31:36 -0700

    added attention layer

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Author: Brandon Trabucco, Copyright 2019"""
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRED_PACKAGES = [
+    'tensorflow-gpu==2.0.0b1',
+    'nltk',
+    'matplotlib',
+    'numpy'
+]
+
+
+setup(
+    name='ordered_attention',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
+    packages=[
+        p for p in find_packages() if p.startswith('ordered_attention')
+    ],
+    description='An ordered multi-head attention mechanism.'
+)
