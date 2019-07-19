f479dcfdf13ec824fcdd5e35cd2b2fb176782d3c
Brandon <brandon@btrabucco.com>
2019-07-18 23:31:00 -0700

    working on tests

--- scripts/test_attention_layer.py
+++ scripts/test_attention_layer.py
@@ -6,16 +6,20 @@ from ordered_attention.attention_layer import AttentionLayer
 
 if __name__ == "__main__":
 
+    inputs = tf.random.normal([1, 7, 16, 16])
+    targets = tf.random.normal([1, 7, 16, 16])
+
     layer = AttentionLayer(2, 2, 4, 16)
-    data = tf.random.normal([1, 7, 16, 16])
-    target = tf.random.normal([1, 7, 16, 16])
+    outputs = layer([inputs, inputs, inputs])
+    print("[{}, {}]".format(outputs.numpy().max(), outputs.numpy().min()))
+
+    assert all(x == y for x, y in zip(targets.shape, outputs.shape))
+
     optimizer = tf.keras.optimizers.Adam()
-
-    for i in range(100):
-
+    for i in range(10):
         loss_function = lambda: tf.reduce_mean(
             tf.losses.mean_squared_error(
-                target,
-                layer([data, data, data])))
+                targets,
+                layer([inputs, inputs, inputs])))
         optimizer.minimize(loss_function, layer.trainable_variables)
-        print("Test Loss: {:0.5f}".format(loss_function().numpy()))
+        print("Test Loss: {}".format(loss_function().numpy()))
